947cddb75fc6c29b1eb8085bf637465319c8813c
Juan Camilo Munoz <jcmunoz@smdigital.com.co>
2021-05-22 17:59:20 -0500

    Se añade estado a la section de comentarios

--- src/components/Quotes/QuoteItem/index.js
+++ src/components/Quotes/QuoteItem/index.js
@@ -3,23 +3,36 @@ import PropTypes from 'prop-types'
 import CustomListAdd from "components/Buttons/CustomListAdd";
 import RatingButtonList from "components/Buttons/RatingButtonList";
 import Comments from "components/Comments/CommentSection";
+import { useState } from "react";
 
 const QuoteItem = ({ quoteId, quote }) => {
+
+  const [activeComment, setActiveComment] = useState(false)
+
+  const handleCommentClick = () => {
+    setActiveComment(!activeComment)
+  }
+
   return (
     <Segment size="big">
       {quote}
       <Divider fitted hidden />
       <RatingButtonList />
 
-      <Button size="small" color="blue" >
+      <Button
+        size="small"
+        color="blue"
+        onClick={handleCommentClick}
+      >
         <Icon name="comment alternate outline" />
         Comentarios
       </Button>
 
       <CustomListAdd />
 
-
-      {/* <Comments /> */}
+      {
+        activeComment && <Comments />
+      }
       
     </Segment>
   );
