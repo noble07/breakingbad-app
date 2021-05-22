530cb10af2a99a317eb4c04a5a2a77056b637a13
Juan Camilo Munoz <jcmunoz@smdigital.com.co>
2021-05-22 13:32:00 -0500

    Se ajusta active item del menu

--- src/components/MenuApp/index.js
+++ src/components/MenuApp/index.js
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
 
 const MenuApp = () => {
+
+  const location = useLocation()
+
   return (
     <Menu color="green" size="huge" inverted secondary>
-      <Menu.Item>
+      <Menu.Item active={ location.pathname === '/' } >
         <Link to="/">
           Breaking Bad App
         </Link>
       </Menu.Item>
-      <Menu.Item>
+      <Menu.Item active={ location.pathname === '/lists' }>
         <Link to="/lists">
           Listas personalizadas 
         </Link>
